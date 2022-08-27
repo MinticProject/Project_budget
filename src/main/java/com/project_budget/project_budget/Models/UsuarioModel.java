8401392d533c41d443b7d0f2230b289ef07aabd4
Juan Manuel Mendoza <juanmanuelmendoza0405@gmail.com>
2022-08-27 15:16:29 -0500

    Creación clase Usuario

---
+++ src/main/java/com/project_budget/project_budget/Models/UsuarioModel.java
@@ -0,0 +1,35 @@
+package com.project_budget.project_budget.Models;
+
+import javax.persistence.*;
+import javax.persistence.Id;
+@Entity
+@Table(name="usuario")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true,nullable = false)
+
+    private Long id;
+    private String usuario; 
+    private String contraseña;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
