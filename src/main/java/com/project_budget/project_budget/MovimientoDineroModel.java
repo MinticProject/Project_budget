6e79dbabe2ab039d6bc3184e0141ca2f8ea3ef4c
DuvanDeschamps <duvan.desm2@gmail.com>
2022-08-27 09:21:35 -0500

    Creacion de la clase MovimientoDinero

---
+++ src/main/java/com/project_budget/project_budget/MovimientoDineroModel.java
@@ -0,0 +1,45 @@
+package com.project_budget.project_budget;
+
+    @Entity
+    @Table(name = "movimientodinero")
+    public class MovimientoDineroModel {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(unique = true,nullable = false)
+        private Long id;
+
+        private float monto;
+        private String concepto;
+
+        //relacion una a muchos con la tabla empresa
+        @ManyToOne
+        @JoinColumn(name="empleado_id",referencedColumnName="id", insertable=false, updatable=false
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+                
+                
+    }
+
