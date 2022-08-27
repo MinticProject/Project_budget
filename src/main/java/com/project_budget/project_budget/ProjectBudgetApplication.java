590e3b117afa16797b7bf5cae2cc87ec47df489e
CamiloPuche <camilopuche73@gmail.com>
2022-08-26 20:47:47 -0500

    Proyecto presupuesto 1.0

---
+++ src/main/java/com/project_budget/project_budget/ProjectBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.project_budget.project_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_budget/project_budget/ProjectBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_budget.project_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
