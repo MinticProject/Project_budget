803c662022c33573b069337bc5b2f59890162a55
Jaimecd2022 <111260318+Jaimecd2022@users.noreply.github.com>
2022-08-27 09:00:21 -0500

    Creación de la clase Rol

---
+++ src/main/java/com/project_budget/project_budget/RolModel.java
@@ -0,0 +1,38 @@
+
+package com.apispring2.apispring2.Models;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rol")
+public class RolModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String rol;
+    @OneToMany(mappedBy = "rol_id")
+    private List<EmpleadoModel> empleado;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    public List<EmpleadoModel> getEmpleado() {
+        return empleado;
+    }
+    public void setEmpleado(List<EmpleadoModel> empleado) {
+        this.empleado = empleado;
+    }
+
+}
